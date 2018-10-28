66147dd1b2bf2813164be638850c077bd2863612
vijayshire <vijayhire@rediffmail.com>
2018-10-28 18:11:07 +0530

    new pages added

--- WebProject/src/main/java/loginBean/LoginBean.java
+++ WebProject/src/main/java/loginBean/LoginBean.java
@@ -1,23 +1,21 @@
-package test;
+package loginBean;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorld
+ * Servlet implementation class LoginBean
  */
-@WebServlet("/hello")
-public class HelloWorld extends HttpServlet {
+public class LoginBean extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorld() {
+    public LoginBean() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,8 +26,6 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/plain");
-	      response.getWriter().write("Hello World! Maven Web Project Example.");
 	}
 
 	/**
